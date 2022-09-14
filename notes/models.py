8cad4a5180111a6052f0ff24aa3a463fa4a73a1f
Tydenius <victort.cordeiro@outlook.com.br>
2022-09-14 11:06:24 -0300

    commit 14/09 tabela tag criada foreign key de tag em note feito insert e update atualizado para inserir tag nova

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,26 @@
+from operator import truediv
 from django.db import models
+
+    
+class Tag(models.Model):
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        string = f"{self.name}"
+        return string
+
+    class Meta:
+        ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
 
 class Note(models.Model):
 
     title = models.CharField(max_length=200)
     content = models.TextField()
+    tag = models.ForeignKey(Tag, default=0, on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         string = f"{self.id}. {self.title}"
--- notes/views.py
+++ notes/views.py
@@ -1,17 +1,37 @@
 from operator import methodcaller
 from django.shortcuts import render, redirect,get_object_or_404
-from .models import Note
+from .models import Note,Tag
 
 
 def index(request):
     if request.method == 'POST':
-            title = request.POST.get('titulo')
-            content = request.POST.get('detalhes')
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        tag = request.POST.get('tag')
+        
+        try:
+                check = Tag.objects.only('id').get(name=tag).id
+        except Tag.DoesNotExist:
+                check = 0
 
-            insert = Note(title=f'{title}', content= f'{content}' )
-            insert.save()
+        if check != 0:
+                obj = Tag.objects.get(id=check)
+                insert = Note(title=f'{title}', content= f'{content}', tag=obj )
+                insert.save()
 
-            return redirect('index')
+        else:
+                insert = Tag(name=tag)
+                insert.save()
+
+                tag_id = Tag.objects.only('id').get(name=tag).id
+                obj = Tag.objects.get(id=tag_id)
+
+                insert = Note(title=f'{title}', content= f'{content}', tag=obj )
+                insert.save()
+
+            
+            
+        return redirect('index')
             
                
     else:
@@ -21,13 +41,33 @@ def index(request):
 
 def index_update(request, id):
    if request.method == 'POST':
-            title = request.POST.get('titulo')
-            content = request.POST.get('detalhes')
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        tag = request.POST.get('tag')
 
-            insert = Note(id=id, title=f'{title}', content= f'{content}' )
-            insert.save()
+        try:
+                check = Tag.objects.only('id').get(name=tag).id
+        except Tag.DoesNotExist:
+                check = 0
+        
+        if check != 0:
+                obj = Tag.objects.get(id=check)
+                insert = Note(id=id, title=f'{title}', content= f'{content}', tag=obj )
+                insert.save()
+        else:
+                insert = Tag(name=tag)
+                insert.save()
 
-            return redirect('index')
+                tag_id = Tag.objects.only('id').get(name=tag).id
+                obj = Tag.objects.get(id=tag_id)
+
+                insert = Note(id=id, title=f'{title}', content= f'{content}', tag=obj )
+                insert.save()
+                
+
+        
+
+        return redirect('index')
 
 def index_delete(request, id):
    if request.method == 'POST':
