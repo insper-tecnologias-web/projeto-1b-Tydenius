3f0b981fd902341177c3e72449c947bd91b8fca4
Tydenius <victort.cordeiro@outlook.com.br>
2022-09-23 18:08:31 -0300

    fixes to css, settings

--- getit/settings.py
+++ getit/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$acl90erz869+a=(h7v#(jy8cb2+2)qrtduwuuu7t#_zlcu=j_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['young-shore-89677.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
