4550537b072cc4ae2ae55fe6ee69aba356bd3b18
Tydenius <victort.cordeiro@outlook.com.br>
2023-03-28 16:05:46 -0300

    changes

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate && python manage.py collectstatic --noinput
-web: gunicorn web.wsgi
+web: gunicorn getit.wsgi
--- getit/settings.py
+++ getit/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$acl90erz869+a=(h7v#(jy8cb2+2)qrtduwuuu7t#_zlcu=j_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://web-production-cfda.up.railway.app','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-cfda.up.railway.app','localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://web-production-cfda.up.railway.app']
 
 
