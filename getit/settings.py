3e03bc8234e0a1c9cfadae7c7fc0552b4dff2b41
Tydenius <victort.cordeiro@outlook.com.br>
2023-03-28 15:40:13 -0300

    fix trusted

--- getit/settings.py
+++ getit/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$acl90erz869+a=(h7v#(jy8cb2+2)qrtduwuuu7t#_zlcu=j_
 DEBUG = False
 
 ALLOWED_HOSTS = ['web-production-cfda.up.railway.app','localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['web-production-cfda.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-cfda.up.railway.app']
 
 
 # Application definition
